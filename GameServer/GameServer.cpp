4feeebb0095e45fceb76206caece691f1c72e543
AzureAD\강성현 <kangs1012@naver.com>
2023-05-01 23:34:14 +0900

    commit to remember condition_variable

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -20,13 +20,14 @@ void Producer()
 	// 4) 조건 변수를 통해 다른 쓰레드에 통지
 	while (true)
 	{
+		// bracket 탈출 시 lock 해제
 		{
 			unique_lock<mutex> lock(m);
 			q.push(100);
 		}
 
 		// wait 중인 쓰레드 1개를 깨운다.
-		cv.notify_one(); 
+		cv.notify_one();
 	}
 }
 
@@ -35,19 +36,20 @@ void Consumer()
 	while (true)
 	{
 		unique_lock<mutex> lock(m);
+
 		// 1) lock
 		// 2) 조건 확인
 		//  - true => 이어서 코드 진행
 		//  - false => unlock 후 대기 상태
 		cv.wait(lock, []() { return q.empty() == false; });
 
-		if (q.empty() == false)
-		{
-			int32 data = q.front();
-			q.pop();
+		// notify_one => 조건식 항상 만족?
+		// Spurious Wakeup (가짜기상)
+		// notify_one 할 때 lock을 잡고 있지 않기 때문에 cross-checking
 
-			cout << data << '\n';
-		}
+		int32 data = q.front();
+		q.pop();
+		cout << data << ' ' << q.size() << '\n';
 	}
 }
 
@@ -58,18 +60,10 @@ int main()
 	// Usage Count
 	// Signal / Non-Signal << bool
 	// Auto / Manual << bool
-	handle = ::CreateEvent(/*보안속성*/NULL, /*bManualReset*/ FALSE, /*bInitialState*/FALSE, /*이름 필수x*/NULL);
-
-	if (handle == NULL)
-	{
-		return 0;
-	}
 	
 	thread t1(Producer);
 	thread t2(Consumer);
 
 	t1.join();
 	t2.join();
-
-	::CloseHandle(handle);
 }
