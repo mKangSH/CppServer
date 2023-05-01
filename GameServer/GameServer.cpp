dc225476d656d9e8d28dd862fce18c6f5d4d317f
AzureAD\강성현 <kangs1012@naver.com>
2023-05-02 00:36:51 +0900

    Future, promise, packaged_task 기록용 커밋

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -3,67 +3,130 @@
 #include <thread>
 #include <atomic>
 #include <mutex>
-#include <Windows.h>
+#include <future>
 
-mutex m;
-queue<int32> q;
-HANDLE handle = NULL;
+int64 result;
 
-// User-Level Object(표준 mutex와 짝지어 동작)
-condition_variable cv;
-
-void Producer()
+int64 Calculate()
 {
-	// 1) lock
-	// 2) 공유 변수 값 수정
-	// 3) unlock
-	// 4) 조건 변수를 통해 다른 쓰레드에 통지
-	while (true)
-	{
-		// bracket 탈출 시 lock 해제
-		{
-			unique_lock<mutex> lock(m);
-			q.push(100);
-		}
+	int64 sum = 0;
 
-		// wait 중인 쓰레드 1개를 깨운다.
-		cv.notify_one();
+	for (int32 i = 0; i < 1'000'000'000; i++)
+	{
+		sum += i;
 	}
+
+	result = sum;
+
+	return sum;
 }
 
-void Consumer()
+class Knight
 {
-	while (true)
-	{
-		unique_lock<mutex> lock(m);
+public:
+	int64 GetHP() { return _HP; }
+private:
+	int64 _HP = 100;
+};
 
-		// 1) lock
-		// 2) 조건 확인
-		//  - true => 이어서 코드 진행
-		//  - false => unlock 후 대기 상태
-		cv.wait(lock, []() { return q.empty() == false; });
-
-		// notify_one => 조건식 항상 만족?
-		// Spurious Wakeup (가짜기상)
-		// notify_one 할 때 lock을 잡고 있지 않기 때문에 cross-checking
-
-		int32 data = q.front();
-		q.pop();
-		cout << data << ' ' << q.size() << '\n';
-	}
+void PromiseWorker(std::promise<string>&& promise)
+{
+	promise.set_value("Secret message!");
 }
 
+void TaskWorker(std::packaged_task<int64(void)>&& task)
+{
+	task();
+}
 
 int main()
 {
-	// Kernel Object
-	// Usage Count
-	// Signal / Non-Signal << bool
-	// Auto / Manual << bool
-	
-	thread t1(Producer);
-	thread t2(Consumer);
 
-	t1.join();
-	t2.join();
+	// 동기 (synchronous) 실행
+	/*
+	int64 sum = Calculate();
+	cout << sum << endl;
+	*/
+
+	// 비동기 실행
+
+	// thread 방식
+	/*
+	{
+		
+		thread t(Calculate);
+		//
+		// TODO
+		//
+		t.join();
+		
+	}
+	*/
+	
+	// std::future : 중요도는 낮지만 시간이 오래걸리는 함수 처리에 유용
+	{
+		// async 옵션
+		// 1) deferred -> lazy evaluation 지연 실행 (command pattern)
+		// 2) async -> 별도의 쓰레드 생성하여 실행
+		// 3) deferred | async -> 둘 중 컴파일러가 선택하여 실행
+
+		// 미래에 결과물을 반드시 뱉는다는 의미
+		std::future<int64> future1 = std::async(std::launch::async, Calculate);
+		
+		Knight knight;
+		// 멤버 함수는 객체에 의존하기 때문에 객체도 포함하여 async 호출
+		std::future<int64> future2 = std::async(std::launch::async, &Knight::GetHP, knight); // knight.GetHP();
+
+		// TODO
+		for (int i = 0; i < 1'000'000; i++)
+		{
+			this_thread::sleep_for(300ns);
+		}
+
+		cout << "knight HP: ";
+		int64 knightHP = future2.get();
+		cout << knightHP << endl;
+		
+		cout << "1부터 1,000,000,000까지의 합: ";
+		int64 sum = future1.get(); // 완료될 때까지 기다려준다. 
+		cout << sum << endl;
+	}
+
+	// std::promise
+	{
+		// 미래 결과물을 반환할거라 약속 [계약서]
+		// promise는 서브 쓰레드로 future는 주 쓰레드에서 보유
+		std::promise<string> promise1; 
+		std::future<string> future1 = promise1.get_future();
+
+		thread t(PromiseWorker, std::move(promise1));
+
+		// TODO
+
+		// future.get() 하고 난 이후엔 future1의 값이 null이 되어버림 2번 이상 호출 x
+		string message1 = future1.get();
+		cout << message1 << endl;
+
+		t.join();
+	}
+
+	// std::packaged_task
+	{
+		std::packaged_task<int64(void)> task(Calculate);
+		std::future<int64> future1 = task.get_future();
+
+		thread t(TaskWorker, std::move(task));
+
+		// TODO
+
+		cout << "1부터 1,000,000,000까지의 합: ";
+		int64 res = future1.get();
+		cout << res << endl;
+
+		t.join();
+	}
+
+	// 결론)
+	// mutex, condition_variable까지 가지 않고 단순한 일을 처리할 때 사용 가능함
+	// 특히 단발성 이벤트에 유용.
 }
