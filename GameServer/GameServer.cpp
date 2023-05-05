26148c388153f3436abaa75273a849c14f44e870
AzureAD\강성현 <kangs1012@naver.com>
2023-05-05 13:34:06 +0900

    Record Lock-Based queue/stack

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -5,33 +5,43 @@
 #include <mutex>
 #include <future>
 #include <Windows.h>
+#include "ConcurrentQueue.h"
+#include "ConcurrentStack.h"
 
-int32 GValue;
-thread_local int32 LThreadId = 0;
-thread_local queue<int32> q;
+LockQueue<int32> q;
+LockStack<int32> s;
 
-void ThreadMain(int32 threadId)
+void Push()
 {
-	LThreadId = threadId;
-
 	while (true)
 	{
-		cout << "Thread ID: " << LThreadId << endl;
-		this_thread::sleep_for(1s);
+		int32 value = rand() % 100;
+		q.Push(value);
+
+		this_thread::sleep_for(10ms);
 	}
 }
+
+void Pop()
+{
+	while (true)
+	{
+		int32 data = 0;
+
+		if (q.WaitPop(OUT data))
+		{
+			cout << data << endl;
+		}
+	}
+}
+
 int main()
 {
-	vector<thread> threads;
+	thread t1(Push);
+	thread t2(Pop);
+	thread t3(Pop);
 
-	for (int32 i = 0; i < 10; i++)
-	{
-		int32 threadId = i + 1;
-		threads.push_back(thread(ThreadMain, threadId));
-	}
-
-	for (thread& t : threads)
-	{
-		t.join();
-	}
+	t1.join();
+	t2.join();
+	t3.join();
 }
