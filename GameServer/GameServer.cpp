b72ec22646fc887ab817b866a439b3e157d46f75
AzureAD\강성현 <kangs1012@naver.com>
2023-05-07 14:58:30 +0900

    server main 초기화

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -7,32 +7,7 @@
 #include <future>
 #include "ThreadManager.h"
 
-#include "AccountManager.h"
-#include "PlayerManager.h"
-
-CoreGlobal coreGlobal;
-
 int main()
 {
-	GThreadManager->Launch([=]
-		{
-			while (true)
-			{
-				cout << "PlayerAccount\n";
-				GPlayerManager.PlayerThenAccount();
-				this_thread::sleep_for(10ms);
-			}
-		});
-
-	GThreadManager->Launch([=]
-		{
-			while (true)
-			{
-				cout << "AccountThenPlayer\n";
-				GAccountManager.AccountThenPlayer();
-				this_thread::sleep_for(10ms);
-			}
-		});
-
-	GThreadManager->Join();
+	
 }
