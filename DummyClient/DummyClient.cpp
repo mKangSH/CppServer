fc84bd82ef69e18a1d2e9141c4360c242335dccb
shkang2 <shkang2@piegroup.co.kr>
2023-08-27 13:22:15 +0900

    Client 및 Server TCP/IP 연결 기초

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -1,7 +1,81 @@
 ﻿#include "pch.h"
 #include <iostream>
 
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+// 매우 중요
+#pragma comment(lib, "ws2_32.lib")
+
+// WSAStartup <=> WSACleanup
 int main()
 {
+	// Initialize ws2_32 Library
+	// 관련 정보가 wsaData에 채워짐
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+	{
+		return -1;
+		::WSACleanup();
+	}
 
+	// tcp => Socket 유실 시 프로토콜 단에서 따로 작업 없이 재전송 및 데이터 오염 체크
+	// udp => 빠른 전송 유실 체크 x
+
+	// ad : Address Family (AF_INET = IPv4, AF_INET6 = IPv6)
+	// type : TCP(SOCK_STERAM) vs UDP(SOCK_DGRAM)
+	// protocol : 0
+	// return : descriptor
+	SOCKET clientSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (clientSocket == INVALID_SOCKET)
+	{
+		// Log Viewer 제작 후 Log 띄우기
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode : " << errCode << endl;
+		::WSACleanup();
+		return -1;
+	}
+
+	// 127.0.0.1 Loop Back IP
+	// Destination = (IP Addr + Port) 
+	SOCKADDR_IN serverAddr; // IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+
+	// htons = host(local computer) to network short
+	// Endian 이슈 때문에 사용 => Big-Endian(network) vs Little-Endian
+	serverAddr.sin_port = ::htons(7777); // 80 : HTTP etc..
+	
+	// Server Address => DNS 교체
+	// serverAddr.sin_addr.s_addr = ::inet_addr("127.0.0.1") << deprecated;
+	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr); 
+
+	// 실질적인 연결 시도부
+	if (::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		// Log Viewer 제작 후 Log 띄우기
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode : " << errCode << endl;
+		::WSACleanup();
+		return -1;
+	}
+
+	// ============================== //
+	// 연결 성공 ! 데이터 송수신 가능 !
+	cout << "Connected To Server!" << endl;
+
+	while (true)
+	{
+		// TODO 
+
+		this_thread::sleep_for(1s);
+	}
+
+	// ============================== //
+
+	// 소켓 리소스 반환
+	::closesocket(clientSocket);
+
+	// WinSock 종료
+	::WSACleanup();
 }
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -5,122 +5,96 @@
 #include <mutex>
 #include <Windows.h>
 #include <future>
-
 #include "ThreadManager.h"
-#include "Allocator.h"
-#include "Memory.h"
-#include "LockFreeStack.h"
+
+#include <WinSock2.h>
+#include <MSWSock.h>
+#include <WS2tcpip.h>
+// 매우 중요
+#pragma comment(lib, "ws2_32.lib")
 
 // CoreGlobal 객체 생성
 CoreGlobal GCoreGlobal;
 
 // 실질적인 코드 작성부
-typedef enum PlayerType
-{
-	KNIGHT,
-	MAGE,
-	ARCHER,
-
-}PlayerType;
-
-
-using TL = TypeList<class Player, class Mage, class Knight, class Archer>;
-
-class Player
-{
-public:
-	Player()
-	{
-		INIT_TL(Player)
-	}
-
-	virtual ~Player() {}
-
-	DECLARE_TL
-};
-
-class Knight : public Player
-{
-public:
-	Knight() { INIT_TL(Knight) }
-};
-
-class Mage : public Player
-{
-public:
-	Mage() { INIT_TL(Mage) }
-};
-
-class Archer : public Player
-{
-public:
-	Archer() { INIT_TL(Archer) }
-};
-
-class Monster
-{
-public:
-	int64 _id = 0;
-};
 
 int main()
 {
-	/*TypeList<Mage, Knight>::Head whoAmI;
-	TypeList<Mage, Knight>::Tail whoAmI2;
-
-	TypeList<Mage, TypeList<Knight, Archer>>::Head whoAmI3;
-	TypeList<Mage, TypeList<Knight, Archer>>::Tail::Head whoAmI4;
-	TypeList<Mage, TypeList<Knight, Archer>>::Tail::Tail whoAmI5;
-
-	int32 len1 = Length<TypeList<Mage, Knight>>::value;
-	int32 len2 = Length<TypeList<Mage, Knight, Archer>>::value;
-	int32 len3 = Length<TypeList<>>::value;
-
-	TypeAt<TL, 0>::Result WhoAmI6;
-	TypeAt<TL, 1>::Result WhoAmI7;
-	TypeAt<TL, 2>::Result WhoAmI8;
-
-	int32 index1 = IndexOf<TL, Mage>::value;
-	int32 index2 = IndexOf<TL, Knight>::value;
-	int32 index3 = IndexOf<TL, Archer>::value;
-	int32 index4 = IndexOf<TL, Monster>::value;
-
-	bool canConvert1 = Conversion<Player, Knight>::exists;
-	bool canConvert2 = Conversion<Knight, Player>::exists;
-	bool canConvert3 = Conversion<Knight, Monster>::exists;
-	
-	TypeConversion<TL> test;*/
-
-	//{
-	//	Player* player = new Knight();
-
-	//	bool canCast = CanCast<Knight*>(player);
-	//	Knight* knight = TypeCast<Knight*>(player);
-	//	
-	//	if (knight)
-	//	{
-	//		// ~~
-	//	}
-
-	//	delete player;
-	//}
-
+	// Initialize ws2_32 Library
+	// 관련 정보가 wsaData에 채워짐
+	WSAData wsaData;
+	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
 	{
-		shared_ptr<Player> player = MakeShared<Knight>();
-
-		shared_ptr<Archer> archer = TypeCast<Archer>(player);
-		bool canCast = CanCast<Mage>(player);
+		return -1;
+		::WSACleanup();
 	}
 
-	for (int32 i = 0; i < 5; i++)
+	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
+	if (listenSocket == INVALID_SOCKET)
 	{
-		GThreadManager->Launch([]()
-			{
-				while (true)
-				{
-				}
-			});
+		// Log Viewer 제작 후 Log 띄우기
+		int32 errCode = ::WSAGetLastError();
+		cout << "Socket ErrorCode : " << errCode << endl;
+		::WSACleanup();
+		return -1;
 	}
 
-	GThreadManager->Join();
+	SOCKADDR_IN serverAddr; // IPv4
+	::memset(&serverAddr, 0, sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = ::htons(7777); // 80 : HTTP etc..
+	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
+
+	if(::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	{
+		// Log Viewer 제작 후 Log 띄우기
+		int32 errCode = ::WSAGetLastError();
+		cout << "Bind ErrorCode : " << errCode << endl;
+		::WSACleanup();
+		return -1;
+	}
+
+	const int backLog = 10; // 대기열 Capacity
+	if (::listen(listenSocket, backLog) == SOCKET_ERROR)
+	{
+		// Log Viewer 제작 후 Log 띄우기
+		int32 errCode = ::WSAGetLastError();
+		cout << "Listen ErrorCode : " << errCode << endl;
+		::WSACleanup();
+		return -1;
+	}
+
+	// ================================================
+	// Data 송수신 시작!
+
+	while (true)
+	{
+		SOCKADDR_IN clientAddr; // IPv4 IP Ban 등에 사용하게 될 중요한 정보!
+		::memset(&clientAddr, 0, sizeof(serverAddr));
+		int32 addrLen = sizeof(clientAddr);
+
+		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+		if (clientSocket == INVALID_SOCKET)
+		{
+			// Log Viewer 제작 후 Log 띄우기
+			int32 errCode = ::WSAGetLastError();
+			cout << "Accept ErrorCode : " << errCode << endl;
+			::WSACleanup();
+			return -1;
+		}
+
+		// client Data 수신 완료!
+		
+		// IP 찍어보고 싶다!
+		char ipAddress[16];
+		::inet_ntop(AF_INET, &clientAddr.sin_addr, ipAddress, sizeof(ipAddress));
+		cout << "Client Entry! IP = " << ipAddress << endl;
+
+		// TODO
+	}
+
+	// ===============================================
+
+	// WinSock 종료
+	::WSACleanup();
 }
