8cbad9a84304d97862f403db8431a0400627c04a
shkang2 <shkang2@piegroup.co.kr>
2023-09-03 20:42:51 +0900

    Select Model 기록

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -24,6 +24,16 @@ void HandleError(const char* cause)
 	::WSACleanup();
 }
 
+const int32 BUFSIZE = 1000;
+
+struct Session
+{
+	SOCKET socket = INVALID_SOCKET;
+	char recvBuffer[BUFSIZE] = {};
+	int32 recvBytes = 0;
+	int32 sendBytes = 0;
+};
+
 int main()
 {
 	WSAData wsaData;
@@ -32,20 +42,12 @@ int main()
 		return -1;
 	}
 
-	// 블로킹(Blocking) 소켓, TCP 기준
-	// accept -> 접속한 클라가 있을 때
-	// connect -> 서버 접속 성공했을 때
-	// send, sendto -> 요청한 데이터를 송신 버퍼에 복사했을 때
-	// recv, recvfrom -> 수신 버퍼에 도착한 데이터가 있고, 이를 유저레벨 버퍼에 복사했을 때
-
-	// 논블로킹(Non-Blocking) 소켓
 	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
 	if (listenSocket == INVALID_SOCKET)
 	{
 		return -1;
 	}
 
-	// non-blocking 세팅
 	u_long on = 1;
 	if (::ioctlsocket(listenSocket, FIONBIO, &on) == INVALID_SOCKET)
 	{
@@ -68,71 +70,122 @@ int main()
 		return -1;
 	}
 
-	cout << "ACCEPT" << endl;
+	cout << "Accept!" << endl;
 
-	SOCKADDR_IN clientAddr;
-	int32 addrLen = sizeof(clientAddr);
+	// 입출력 모델은 Client에서 보내는 모델까지 이해하기 위해 iocp 이외 간단한 모델도 이해해야 함
+	// Select 모델 = (select 함수가 핵심이 되는 모델)
+	// Socket 함수 호출 성공 시점을 미리 알 수 있다.
+
+	// 문제 상황)
+	// 수신 버퍼에 데이터가 존재하지 않는데, read
+	// 송신 버퍼가 full 인데, write
+	// - 블로킹 소켓 : 조건이 만족되지 않아서 Block 되는 상황 예방
+	// - 논블로킹 소켓 : 조건이 만족되지 않아서 불필요하게 반복 체크하는 상황 예방
+
+	// socket set
+	// 1) 읽기[ 1 2 3 ] 쓰기[ ] 예외(OOB)[ ] 관찰 대상 등록
+	// OutOfBand는 send() 마지막 인자 MSG_OOB로 보내는 특별한 데이터
+	// 받는 쪽에서도 recv OOB 세팅을 해야 읽을 수 있음.
+	// 2) select(readSet, writeSet, exceptSet); -> 관찰 시작
+	// 3) 적어도 하나의 소켓이 준비되면 리턴 -> 낙오자는 알아서 제거
+	// 4) 남은 소켓 체크해서 진행
+
+	// fd_set set;
+	// FD_ZERO : 비운다
+	// ex) FD_ZERO(&set); 
+	// FD_SET : 소켓 s를 넣는다
+	// ex) FD_SET(s, &set);
+	// FD_CLR : 소켓 s를 제거
+	// ex) FD_CLR(s, &set);
+	// FD_ISSET : 소켓 s가 set에 들어있으면 0이 아닌 값을 리턴한다.
+	// ex) FD_ISSET(s, &set);
+
+	// Select Model 한계:
+
+	vector<Session> sessions;
+	sessions.reserve(100);
+
+	fd_set reads;
+	fd_set writes;
 
 	while (true)
 	{
-		SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
-		if (clientSocket == INVALID_SOCKET)
+		// 소켓 셋 초기화
+		FD_ZERO(&reads);
+		FD_ZERO(&writes);
+
+		// listenSocket 등록
+		FD_SET(listenSocket, &reads);
+
+		for (Session& s : sessions)
 		{
-			// 원래 블록했어야 하는데? 아니니깐
-			if (::WSAGetLastError() == WSAEWOULDBLOCK)
+			if (s.recvBytes <= s.sendBytes)
 			{
-				continue;
+				FD_SET(s.socket, &reads);
 			}
 
-			// Error
+			else
+			{
+				FD_SET(s.socket, &writes);
+			}
+		}
+
+		// [option] 마지막 timeout 인자 설정 가능
+		// timeval timeout;
+		// timeout.tv_sec;
+		// timeout.tv_usec;
+		int32 retVal = ::select(0, &reads, &writes, nullptr, nullptr);
+		if (retVal == SOCKET_ERROR)
+		{
 			break;
 		}
 
-		cout << "Client Connected! " << endl;
-
-		// Recv
-		while (true)
+		// Listener 소켓 체크
+		if (FD_ISSET(listenSocket, &reads))
 		{
-			char recvBuffer[1000];
-			int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-			if (recvLen == SOCKET_ERROR)
+			SOCKADDR_IN clientAddr;
+			int32 addrLen = sizeof(clientAddr);
+			SOCKET clientSocket = ::accept(listenSocket, (SOCKADDR*)&clientAddr, &addrLen);
+			if (clientSocket != INVALID_SOCKET)
 			{
-				// 원래 블록했어야 하는데? 아니니깐
-				if (::WSAGetLastError() == WSAEWOULDBLOCK)
+				cout << "Client Connected" << endl;
+				sessions.push_back(Session{ clientSocket });
+			}
+		}
+
+		// 나머지 소켓 체크
+		for (Session& s : sessions)
+		{
+			if (FD_ISSET(s.socket, &reads))
+			{
+				int32 recvLen = ::recv(s.socket, s.recvBuffer, BUFSIZE, 0);
+				if (recvLen <= 0)
 				{
+					// TODO :: sessions 제거
 					continue;
 				}
 
-				// Error
-				break;
+				s.recvBytes = recvLen;
 			}
 
-			else if (recvLen == 0)
+			else if (FD_ISSET(s.socket, &writes))
 			{
-				// 연결 끊김 
-				break;
-			}
+				// Blocking Mode -> 모든 데이터 전송
+				// Non-Blocking Mode -> 일부 데이터 전송 가능성이 있음 (상대방 수신 버퍼 상황에 따라)
+				int32 sendLen = ::send(s.socket, &s.recvBuffer[s.sendBytes], s.recvBytes - s.sendBytes, 0);
 
-			cout << "Recv Data Len = " << recvLen << endl;
-
-			// Send
-			while (true)
-			{
-				if (::send(clientSocket, recvBuffer, recvLen, 0) == SOCKET_ERROR)
+				if (sendLen == SOCKET_ERROR)
 				{
-					// 원래 블록했어야 하는데? 아니니깐
-					if (::WSAGetLastError() == WSAEWOULDBLOCK)
-					{
-						continue;
-					}
-
-					// Error
-					break;
+					// TODO :: sessions 제거
+					continue;
 				}
 
-				cout << "Send Data! Len = " << recvLen << endl;
-
-				break;
+				s.sendBytes += sendLen;
+				if (s.recvBytes == s.sendBytes)
+				{
+					s.recvBytes = 0;
+					s.sendBytes = 0;
+				}
 			}
 		}
 	}
