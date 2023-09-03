7de08595f357fe4b0190e70e12b58750427500a7
shkang2 <shkang2@piegroup.co.kr>
2023-09-03 18:58:56 +0900

    socket option 기록

--- DummyClient/DummyClient.cpp
+++ DummyClient/DummyClient.cpp
@@ -33,68 +33,6 @@ int main()
 		return -1;
 	}
 
-	// 127.0.0.1 Loop Back IP
-	// Destination = (IP Addr + Port) 
-	SOCKADDR_IN serverAddr; // IPv4
-	::memset(&serverAddr, 0, sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = ::htons(7777);
-	::inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
-
-	// Connected UDP(보낼 대상 즐겨찾기 동작 자체는 Unconnected UDP와 동일)
-	// 실질적인 Connection X
-	::connect(clientSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr));
-
-	// ============================== //
-	// 연결 성공 ! 데이터 송수신 가능 !
-
-	cout << "Connected To Server!" << endl;
-
-	while (true)
-	{
-		// TODO 
-		char sendBuffer[100] = "Hello World!";
-
-		// 나의 IP 주소 + 포트 번호 설정 (tcp의 경우 connect에서 설정됨)
-		
-		// Unconnected UDP
-		// int32 resultCode = ::sendto(clientSocket, sendBuffer, sizeof(sendBuffer), 0, 
-		//						(SOCKADDR*)&serverAddr, sizeof(serverAddr));
-
-		int32 resultCode = ::send(clientSocket, sendBuffer, sizeof(sendBuffer), 0);
-		if (resultCode == SOCKET_ERROR)
-		{
-			HandleError("SendTo");
-			return -1;
-		}
-		cout << "Send Data! Data = " << sendBuffer << endl;
-		cout << "Send Data! Len = " << sizeof(sendBuffer) << endl;
-
-		SOCKADDR_IN recvAddr;
-		::memset(&recvAddr, 0, sizeof(recvAddr));
-		int32 AddrLen = sizeof(recvAddr);
-
-		char recvBuffer[1000];
-
-		// Unconnected UDP
-		// int32 recvLen = ::recvfrom(clientSocket, recvBuffer, sizeof(recvBuffer), 0,
-		//						(SOCKADDR*)&recvAddr, &AddrLen);
-
-		int32 recvLen = ::recv(clientSocket, recvBuffer, sizeof(recvBuffer), 0);
-		if (recvLen <= 0)
-		{
-			HandleError("RecvFrom");
-			return 0;
-		}
-
-		cout << "Recv Data! Data = " << recvBuffer << endl;
-		cout << "Recv Data! Len = " << recvLen << endl;
-
-		this_thread::sleep_for(1s);
-	}
-
-	// ============================== //
-
 	// 소켓 리소스 반환
 	::closesocket(clientSocket);
 
--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -41,49 +41,72 @@ int main()
 		return -1;
 	}
 
-	SOCKADDR_IN serverAddr; // IPv4
-	::memset(&serverAddr, 0, sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_port = ::htons(7777);
-	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	// 옵션을 해석하고 처리할 주체 -> Level
+	//    level -> KEEPALIVE, LINGER, RCVBUF, REUSEADDR, SNDBUF 공부해보기
+	// 소켓 코드 -> SOL_SOCKET
+	// IPv4 -> IPPROTO_IP
+	// TCP 프로토콜 -> IPPROTO_IP
 
-	if (::bind(serverSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
+	// SO_KEEPALIVE = 주기적인 연결상태 확인 여부(TCP Only)
+	// 상대방이 연결을 일방적으로 통보 없이 끊은 경우
+	// 주기적으로 TCP 프로토콜 연결 상태 확인 -> 감지하고 싶을 경우 KEEPALIVE 사용
+
+	bool enable = true;
+	::setsockopt(serverSocket, SOL_SOCKET, SO_KEEPALIVE, (char*)&enable, sizeof(enable));
+
+	// SO_LINGER = 지연하다
+	// 송신 버퍼에 있는 데이터를 보낼 것인가? 날릴 것인가?
+
+	// linger.l_onoff = 0이면 closesocket() 바로 리턴, 아니면 linger초만큼 대기 (default 0)
+	// linger.l_linger : 대기 시간
+	LINGER linger;
+	linger.l_onoff = 1;
+	linger.l_linger = 5;
+	::setsockopt(serverSocket, SOL_SOCKET, SO_LINGER, (char*)&linger, sizeof(linger));
+
+
+	// Half-Close
+	// SD_SEND : send 막는다
+	// SD_RECEIVE : recv 막는다
+	// SD_BOTH : 둘 다 막는다
+	// ::shutdown(serverSocket, SD_SEND);
+
+	// 소켓 리소스 반환
+	// if send -> closesocket 호출된 경우
+	// ::closesocket(serverSocket);
+
+	// SO_RCVBUF = 수신 버퍼 크기
+	int32 recvBufferSize;
+	int32 optionLength = sizeof(recvBufferSize);
+	::getsockopt(serverSocket, SOL_SOCKET, SO_RCVBUF, (char*)&recvBufferSize, &optionLength);
+	cout << "수신 버퍼 크기 : " << recvBufferSize << endl;
+
+	// SO_SNDBUF = 송신 버퍼 크기
+	int32 sendBufferSize;
+	optionLength = sizeof(sendBufferSize);
+	::getsockopt(serverSocket, SOL_SOCKET, SO_SNDBUF, (char*)&sendBufferSize, &optionLength);
+	cout << "송신 버퍼 크기 : " << sendBufferSize << endl;
+
+	// SO_REUSEADDR
+	// IP 주소 및 Port 재사용
+	// 주소가 사용 중이거나, 강제 종료 이후 데이터가 남아있는 경우 => 바인딩 실패
+	// 해당 사항 예방차원으로 REUSEADDR 설정
 	{
-		HandleError("Bind");
-		return -1;
+		bool enable = true;
+		::setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, (char*)&enable, sizeof(enable));
 	}
 
-	while (true)
+	// IPPROTO_TCP <- socket 이 아닌 tcp 단계에서 설정
+	// TCP_NODELAY = Nagle 네이글 알고리즘 작동 여부
+	// 데이터가 충분히 크면 보내고, 그렇지 않으면 충분히 쌓일 때까지 대기
+	// 장점 : 작은 패킷 불필요하게 생성되는 일을 방지
+	// 단점 : 반응 시간 손해 
+	// 게임과 같은 경우 반응 시간이 중요하기 때문에 NoDelay 설정
 	{
-		SOCKADDR_IN clientAddr;
-		::memset(&clientAddr, 0, sizeof(clientAddr));
-		int32 clientAddrLen = sizeof(clientAddr);
-
-		char recvBuffer[1000];
-		int32 recvLen = ::recvfrom(serverSocket, recvBuffer, sizeof(recvBuffer), 0,
-								(SOCKADDR*)&clientAddr, &clientAddrLen);
-
-		if (recvLen <= 0)
-		{
-			HandleError("RecvFrom");
-			return 0;
-		}
-
-		cout << "Recv Data! Data = " << recvBuffer << endl;
-		cout << "Recv Data! Len = " << recvLen << endl;
-		
-		int32 errCode = ::sendto(serverSocket, recvBuffer, recvLen, 0,
-							(SOCKADDR*)&clientAddr, sizeof(clientAddr));
-
-		if (errCode == SOCKET_ERROR)
-		{
-			HandleError("SendTo");
-			return -1;
-		}
-
-		cout << "Send Data! Data = " << recvBuffer << endl;
-		cout << "Send Data! Len : " << recvLen << endl;
+		bool enable = true;
+		::setsockopt(serverSocket, IPPROTO_TCP, TCP_NODELAY, (char*)&enable, sizeof(enable));
 	}
+
 
 	// WinSock 종료
 	::WSACleanup();
